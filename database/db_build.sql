b59784d27133b7c956e330cbad2db0df187e7da5
Yahia <zaky20@hotmail.com>
2017-08-09 15:50:34 +0100

    created database schemas relates #18

---
+++ database/db_build.sql
@@ -0,0 +1,27 @@
+BEGIN;
+
+DROP TABLE IF EXISTS users , posts CASCADE;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  faccer VARCHAR(30) NOT NULL,
+  password TEXT NOT NULL,
+  avatar VARCHAR(10),
+);
+
+CREATE TABLE posts (
+  id SERIAL PRIMARY KEY,
+  user_id SERIAL FOREIGN KEY,
+  post VARCHAR(200) NOT NULL,
+  date DATE NOT NULL,
+);
+
+INSERT INTO users (faccer,encrypted,avatar) VALUES
+  ('Aisha','test' , 'avatar1.jpg' ,) ;
+  ('Yahia', 'test1', 'avatar2.jpg' ,) ;
+
+  INSERT INTO posts (user_id,post,date) VALUES
+    ('1', 'pushed to master .... GUILTY' , '2017-06-27'  ) ;
+    ('2', 'missed a capital S and broke a XHR request for 3 hours',  '2017-06-01') ;
+
+COMMIT;
