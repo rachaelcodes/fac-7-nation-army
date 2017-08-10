b9641913b46dc6b8c8b500e05e6e82c18ada271a
Yahia <zaky20@hotmail.com>
2017-08-10 16:54:03 +0100

    attempt at routers relates 15 relates 8 relates #15 relates #8 relates #7

--- src/handlers.js
+++ src/handlers.js
@@ -9,7 +9,10 @@ const {
   verify
 } = require('jsonwebtoken');
 
-const secret = process.env.SECRET;
+const env = require('env2')('.env');
+const path = require('path');
+
+const SECRET = process.env.SECRET;
 const userDetails = process.env.USERDETAILS;
 const notFoundPage = '<p style="font-size: 10vh; text-align: center;">404!</p>';
 
@@ -17,19 +20,22 @@ const handleHome = (request, response) => {
   readFile(__dirname + "/../Public/index.html", function(error, file) {
     if (error) {
       response.writeHead(500, 'Content-Type:text/html');
-      response.end('<h1>Sorry, our homepage is sleeping</h1>');
+      return response.end('<h1>Sorry, our homepage is sleeping</h1>');
 
     } else {
+      console.log('hone');
       response.writeHead(200, {
         "Content-Type": "text/html"
       });
-      response.end(file);
+      return response.end(file);
     }
   });
 
 }
 
 const handlePublic = (request, response) => {
+
+const fileName = request.url ;
   const filePath = path.join(__dirname, '..', 'public', fileName);
   const extension = fileName.split('.')[1];
   const extensionType = {
@@ -44,33 +50,36 @@ const handlePublic = (request, response) => {
       response.writeHead(500, {
         'Content-Type': 'text/html'
       });
-      response.end('Sorry, we\'ve had a problem');
+      return response.end('Sorry, we\'ve had a problem');
     } else {
       response.writeHead(200, {
         'Content-Type': extensionType[extension]
       });
-      response.end(file);
+      return response.end(file);
     }
   });
 }
 
 const handleLogin = (request, response) => {
 
-  const cookie = sign(userDetails, secret);
+  const cookie = sign(userDetails, SECRET);
+  console.log('cookie',cookie);
   response.writeHead(302, {
     'Location': '/',
-    'Set-Cookie: `jwt=${cookie};HttpOnly`'
+    'Set-Cookie': `jwt=${cookie};HttpOnly`
   });
-  response.end();
-  handleAuth(request, response);
+
+response.end();
+  // handleAuth(request, response);
+  console.log('hi2');
 }
 
 const handleLogout = (request, response) => {
   response.writeHead(302, {
     'Location': '/',
-    'Set-Cookie: `jwt=0;Max-Age=0`'
+    'Set-Cookie': `jwt=0;Max-Age=0`
   });
-  response.end()
+  return response.end()
 }
 
 const handleAuth = (request, response) => {
@@ -80,20 +89,20 @@ const handleAuth = (request, response) => {
       'Content-Type': 'text/plain',
       'Content-Length': message.length
     });
-    response.end(message);
+    return response.end(message);
   }
   if (!request.headers.cookie) return sendError();
 
-  const {
-    jwt
-  } = parse(request.headers.cookie);
+console.log(parse(request.headers.cookie));
+  const { jwt } = parse(request.headers.cookie);
 
   if (!jwt) return sendError();
 
-  return verify(jwt, secret, (err, jwt) => {
+  return verify(jwt, SECRET, (err, jwt) => {
     if (err) {
       return sendError();
     } else {
+      console.log('jwt is', jwt);
       const message = `Welcome! Your user ID is: ${jwt.userId}!`
       response.writeHead(
         200, {
@@ -101,7 +110,7 @@ const handleAuth = (request, response) => {
           'Content-Length': message.length
         }
       );
-      response.end(message)
+      return response.end(message)
     }
   })
 }
@@ -111,10 +120,10 @@ const handleError = (request, response) => {
     404, {
       'Content-Type': 'text/html',
       // What does this do?
-      'Content-Length': notFoundPage.length;
+      'Content-Length': notFoundPage.length
     }
   );
-  response.end(notFoundPage)
+  return response.end(notFoundPage)
 }
 const handlePost = (request, response) => {
 
--- src/router.js
+++ src/router.js
@@ -1,26 +1,23 @@
 const http = require('http');
 const handlers = require('./handlers');
-const fileList = ['index.html','index.js', 'style.css','favicon.ico' ]
+const fileList = ['/index.js', '/style.css', '/favicon.ico']
 
-const router = (request,response)=>{
+const router = (request, response) => {
 
-  if (request.url && request.method ) {
-    switch (`${request.method} ${request.url}`){
-      case: 'GET /':
-        handlers.handleHome(request,response);
-      case: 'POST /login':
-        handlers.handleLogin(request, response);
-      case: 'POST /logout':
-        handlers.handleLogout(request, response);
-      // case: 'GET /auth_check':
-      //   handlers.hanndleAuth(request,response);
+  if (request.url && request.method) {
+    switch (`${request.method} ${request.url}`) {
+      case 'GET /':
+        return handlers.handleHome(request, response);
+      case 'POST /login':
+        return handlers.handleLogin(request, response);
+      case 'POST /logout':
+        return handlers.handleLogout(request, response);
+      default:
+        if (fileList.includes(request.url)) {
+          return handlers.handlePublic(request, response);
+        } else
+          return handlers.handleError(request, response);
     }
-  }
-  else if (fileList.includes(request.url)){
-    handlers.handlePublic(request,response);
-  }
-  else{
-    handlers.handleError(request,response);
   }
 
 }
