90a45ac14b1b074032f55407bc16718046d3d69a
rachaelcodes <rorourke.rsp@gmail.com>
2017-08-10 16:00:27 +0100

    added replace login info

--- src/backendHtml.js
+++ src/backendHtml.js
@@ -1,6 +1,16 @@
 const fs = require('fs');
 const databaseConnection = require('../database/db_connection.js');
 
+const getLoginInfo = (user, cb) => {
+  databaseConnection.query(`SELECT avatar, faccer FROM users WHERE id = ${user};`, (err, res) => {
+    if (err) {
+      cb(err)
+    } else {
+      cb(null, res.rows);
+    }
+  });
+};
+
 const getPosts = (cb) => {
   databaseConnection.query('SELECT users.faccer, users.avatar, posts.post, posts.date FROM users INNER JOIN posts ON users.id = posts.user_id;', (err, res) => {
     if (err) {
@@ -12,7 +22,7 @@ const getPosts = (cb) => {
 };
 
 
-const parseSQL = (data) => {
+const parseCommentSQL = (data) => {
   let replacement = '<!-- display comments here -->\n<section class = "display-comments">\n';
 
   data.forEach((e) => {
@@ -30,15 +40,25 @@ const parseSQL = (data) => {
   replacement += '</section>\n<!-- end of comments -->';
 
   return replacement;
-}
+};
 
-const replaceHTML = (replacement) => {
+const parseLoginSQL = (data) => {
+  let replacement = '<!-- logged in header -->\n<header class="header">\n<img src="';
+  replacement += data[0].avatar;
+  replacement += '" alt="Avatar"><p>Welcome ';
+  replacement += data[0].faccer;
+  replacement += '</p>\n</header>\n<!-- end of logged in header -->';
+
+  return replacement;
+};
+
+const replaceHTML = (replacement, regex) => {
   fs.readFile(__dirname + "/../Public/index.html", 'utf8', (err, data) => {
     if (err) {
       return console.log(err);
     }
 
-    const result = data.replace(/<!-- display comments here -->(\n|.)*<!-- end of comments -->/g, replacement);
+    const result = data.replace(regex, replacement);
 
     fs.writeFile(__dirname + "/../Public/index.html", result, 'utf8', (err) => {
       if (err) return console.log(err);
@@ -46,12 +66,21 @@ const replaceHTML = (replacement) => {
   });
 };
 
-const replacement = () => {
+const replaceComments = () => {
   getPosts((error, result) => {
     if (error) return console.log(error);
-    replaceHTML(parseSQL(result));
+    replaceHTML(parseCommentSQL(result), /<!-- display comments here -->(\n|.)*<!-- end of comments -->/g);
   });
 };
 
-replacement();
-module.exports = { replacement };
+const replaceLogin = (user) => {
+  getLoginInfo(user, (error, result) => {
+    if (error) return console.log(error);
+    replaceHTML(parseLoginSQL(result), /<!-- logged in header -->(\n|.)*<!-- end of logged in header -->/g);
+  });
+};
+
+
+replaceLogin(1);
+replaceComments();
+module.exports = { replaceComments, replaceLogin };
